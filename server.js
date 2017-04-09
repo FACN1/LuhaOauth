979fd6aadfa872014a2a7ed9c2561870606269a3
suha-edris <suha.edris@gmail.com>
2017-04-09 16:14:03 +0300

    redirect to homepage

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const inert = require('inert');
 const fs = require('fs');
 const querystring = require('querystring');
 require('env2')('./config.env')
+const Request = require('request');
 
 const server = new hapi.Server()
 
@@ -34,6 +35,31 @@ server.register([ inert ], (err) => {
     path: '/login',
     handler: (request, reply) => {
       reply.redirect('https://github.com/login/oauth/authorize?client_id='+process.env.CLIENT_ID+'&redirect_uri='+process.env.REDIRECT_URI);
+    }
+  })
+  server.route({
+    method: 'GET',
+    path: '/welcome',
+    handler: (request, reply) => {
+      let code = request.query.code;
+      let data = {
+        client_id: process.env.CLIENT_ID,
+        client_secret: process.env.CLIENT_SECRET,
+        code: code
+      }
+      let options = {
+        method:'POST',
+        json: true,
+        body: data,
+        url: 'https://github.com/login/oauth/access_token'
+      }
+      Request(options, (err,response, body) =>{
+        if(err) throw err;
+        console.log(body);
+        reply(body.access_token);
+      })
+
+      // console.log(code)
     }
   })
 });
